114a09c9bc3031716215c0d0b16419b7417d174c
khaos <dev.ikhaos@gmail.com>
2018-06-29 11:19:46 -0600

    update spec components

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
-require 'shoulda/matchers'
 require 'rake'
 require 'json_spec'
 
@@ -13,7 +12,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 Rails.application.load_tasks
 
 RSpec.configure do |config|
-  Mongo::Logger.logger.level = Logger::WARN
   config.include Rails.application.routes.url_helpers
   config.infer_spec_type_from_file_location!
   config.include Requests::JsonHelpers, type: :request
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,13 @@
 # frozen_string_literal: true
 
-require 'factory_bot'
 require 'database_cleaner'
 require 'simplecov'
-require 'webmock/rspec'
 
 SimpleCov.start 'rails'
-WebMock.allow_net_connect!
 
 RSpec.configure do |config|
   config.order = :random
-  config.include FactoryBot::Syntax::Methods
-end
 
-RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner[:mongoid].strategy = :truncation
     DatabaseCleaner.clean_with(:truncation)
